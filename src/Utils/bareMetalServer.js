5fb7cb5a26755ed1184016b71d4cc86866895d97
cristobalquilimaco <quilimacox1@gmail.com>
2023-11-05 11:52:25 -0500

    Se crea json con la información de los servidores dedicados y se crea componente

--- src/Utils/bareMetalServer.js
+++ src/Utils/bareMetalServer.js
@@ -1,7 +1,7 @@
-const dedicatedServer = [
+const bareMetalServer = [
     {
       id: 1,
-      name: 'Dedicated Servers in Miami, FL - Small',
+      name: 'US/East Coast - Supermicro MicroCloud',
       description: 'More information',
       price: '$60 USD',
       savePrice: '$9.99',
@@ -12,30 +12,30 @@ const dedicatedServer = [
       id: 1,
       name: 'Dedicated Servers in Miami, FL - Medium',
       description: 'More information',
-      price: '$82 USD',
+      price: '$60 USD',
       savePrice: '$9.99',
       savePercentage: 'Save 90%',
-      features: ['Intel Dual Xeon 3.0 Ghz', '8 GB RAM Memory', '1 TB HHD', '1 IP (IPv4)', 'Root access / Remote Desktop', '24/7 support',],
+      features: ['Intel Dual Xeon 3.0 Ghz', '8 GB RAM Memory', '1 TB HHD', '1 IP (IPv4)', 'Root access / Remote Desktop', '24/7 support'],
     },
     {
       id: 1,
       name: 'Dedicated Servers in Miami, FL - Large',
       description: 'More information',
-      price: '$120 USD',
+      price: '$60 USD',
       savePrice: '$9.99',
       savePercentage: 'Save 90%',
-      features: ['Intel Dual Xeon 3.0 Ghz', '16 GB RAM memory', '1TB HHD 1', 'IP (IPv4)', 'Root access / Remote Desktop', '24/7 Support',],
+      features: ['Intel Dual Xeon 3.0 Ghz', '16 GB RAM memory', ' 1TB HHD 1','IP (IPv4)', 'Root access / Remote Desktop', '24/7 Support', ],
     },
     {
       id: 1,
       name: 'Dedicated Servers in Miami, FL - Large',
       description: 'More information',
-      price: '$220 USD',
+      price: '$60 USD',
       savePrice: '$9.99',
       savePercentage: 'Save 90%',
-      features: ['Intel Dual Xeon 3.0 Ghz', '32 GB RAM Memory', '2x1 TB HHD', '1 IP (IPv4)', 'Root access / Remote Desktop', '24/7 support',],
+      features: ['Intel Dual Xeon 3.0 Ghz', '16 GB RAM memory', ' 1TB HHD 1','IP (IPv4)', 'Root access / Remote Desktop', '24/7 Support', ],
     },
 
   ];
 
-  export default dedicatedServer
+  export default bareMetalServer
---
+++ src/Utils/vpsKvm.js
@@ -0,0 +1,41 @@
+const dedicatedServer = [
+    {
+      id: 1,
+      name: 'VPS in Miami, FL - Small',
+      description: 'More information',
+      price: '$14 USD',
+      savePrice: '$9.99',
+      savePercentage: 'Save 90%',
+      features: ['3.3 Ghz Xeon E3-1230 CPU x1 Processor', '1 GB Memory RAM', '20 Hard Drive', '1 (IPv4) IP addresses', 'Unlimited Bandwidth', '24/7 Support',],
+    },
+    {
+      id: 1,
+      name: 'VPS in Miami, FL - Medium',
+      description: 'More information',
+      price: '$20 USD',
+      savePrice: '$9.99',
+      savePercentage: 'Save 90%',
+      features: ['3.3 Ghz Xeon E3-1230 CPU x2 Processor', '2 GB Memory RAM', '50 Hard Drive', '1 (IPv4) IP addresses', 'Unlimited Bandwidth', '24/7 Support'],
+    },
+    {
+      id: 1,
+      name: 'VPS in Miami, FL - Large',
+      description: 'More information',
+      price: '$27 USD',
+      savePrice: '$9.99',
+      savePercentage: 'Save 90%',
+      features: ['3.3 Ghz Xeon E3-1230 CPU x3 Processor', '3 GB Memory RAM', '100 Hard Drive', '1 (IPv4) IP addresses', 'Unlimited Bandwidth', '24/7 Support'],
+    },
+    {
+      id: 1,
+      name: 'VPS in Miami, FL - Extra Large',
+      description: 'More information',
+      price: '$$35 USD',
+      savePrice: '$9.99',
+      savePercentage: 'Save 90%',
+      features: ['3.3 Ghz Xeon E3-1230 CPU x4 Processor', '4 GB Memory RAM', '250 Hard Drive', '1 (IPv4) IP addresses', 'Unlimited Bandwidth', '24/7 Support',],
+    },
+
+  ];
+
+  export default dedicatedServer
