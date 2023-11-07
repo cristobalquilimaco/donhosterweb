ef1e42f9797d4e297d65017f60f023574e289b58
cristobalquilimaco <quilimacox1@gmail.com>
2023-11-07 11:23:07 -0500

    Se organiza codigo de politicas de privacidad y terminos de servicio

--- src/Pages/PrivacyPolicy.jsx
+++ src/Pages/PrivacyPolicy.jsx
@@ -1,73 +1,74 @@
 import "../Pages/styles/about.css";
 import "../assets/img/banner-about.jpg";
 import images from "../Utils/images";
+import "../Pages/styles/TermOfServices.css"
 
 const PrivacyPolicy = () => {
   return (
     <div className="about__box">
-        <h1 className="about__title">THE COMPANY</h1>
-            <h3 className="about__subtitle"> Values stem from our focus on customer well-being</h3>
-    <div className="about__p">
-        <i className="bx bx-check" /> Ethics &nbsp;
-        <i className="bx bx-check" /> Responsibility &nbsp;
-        <i className="bx bx-check" /> Respect
+      <h1 className="about__title">THE COMPANY</h1>
+      <h3 className="about__subtitle">Values stem from our focus on customer well-being</h3>
+      <div className="about__p">
+        <i className="bx bx-check"></i> Ethics &nbsp;
+        <i className="bx bx-check"></i> Responsibility &nbsp;
+        <i className="bx bx-check"></i> Respect
       </div>
+      <div className="about__banner">
+        <img src={images.bannerabout} alt="Banner About" />
+      </div>
+      <div className="term__of__services">
+      <center><strong>PRIVACY AND DATA PROCESSING POLICIES</strong>. </center>
 
-    <div className="about__banner">
-        <img src={images.bannerabout} alt="" />
+      <div className="term__of">
+                        <p>Privacy Policy describes <strong>DonHoster</strong> policies and procedures for handling Customer information and visits to our site. This Privacy Policy has been created with the understanding that Internet technologies are still evolving and Internet business methods continue to change to meet the needs and opportunities of changing technologies. As a result, this Privacy Policy is subject to change as we determine appropriate or necessary.</p>
+                        <p><strong>DonHoster</strong> understands the importance and responsibility that personal data protection entails. We are committed to complying with the data protection laws applicable to the company, including the General Data Protection Regulation <strong>(`GDPR`)</strong> of the European Union.</p>
+                        
+                        <p><strong>1. CUSTOMER INFORMATION</strong></p>
+                        
+                        <p><strong>DonHoster</strong> In the course of serving our Clients, we may acquire, store and transmit communications and information that Clients may consider private or confidential. <strong>DonHoster</strong> Some of this information, such as the Clients name, address, credit card information, etc., is provided to us by Clients in order to obtain the Services. Other information, such as Customer account status, choice of Services, Customer records, etc., is created and maintained by us in the normal course of business.</p>
+                        <p><strong>DonHoster</strong> We may also use cookies, which are small pieces of information that a website may store in a designated file on a users computer for various reasons. In addition, we may store Customer email and other electronic communications as necessary for the transmission and delivery of such communications.</p>
+                        
+                        <p><strong>2. DATA SECURITY</strong></p>
+                        
+                        <p><strong>DonHoster</strong> has the responsibility that we will protect the confidentiality of Client information, account information and personal communications to the greatest extent possible in accordance with the law and the legitimate interests of <strong>DonHoster</strong>, its employees and other Clients who use our Services. and/or the Company. Network. To protect against the loss, misuse and alteration of the information collected, we have established appropriate physical, electronic and managerial procedures.</p>
+                        
+                        <p><strong>3. HOW CUSTOMER INFORMATION IS USED</strong></p>
+                        
+                        <p><strong>DonHoste</strong>r We may use Customer information to provide our Customers with system information or information about new or updated products. We may also share Customer information with selected partners. For example, to provide Customers with information about products that may be of interest to them or to enable them to take advantage of special partner programs.</p>
+                        <p>In addition, <strong>DonHoster</strong> may share our website usage information about visitors to <strong>DonHoster</strong> website with third-party advertising companies for the purpose of targeting our Internet banner ads on <strong>DonHoster</strong> website through the use of pixel tags. . The information collected by our selected partners through the use of these pixel tags is not personally identifiable.</p>
+                        
+                        <p><strong>4. EXCLUDE YOURSELF</strong></p>
+                        
+                        <p><strong>DonHoster</strong> customers may opt out of receiving new or updated product notices from us or our selected partners by sending an email to <strong>admin@donhoster.com</strong>. However, Clients may not opt out of receiving information from us that is essential to maintaining or updating Clients accounts or system information.</p>
+                        
+                        <p><strong>5. MODIFICATIONS</strong></p>
+                        
+                        <p><strong>DonHoster</strong> customers can access and modify their personal information by contacting our administration department by sending an email to <strong>admin@donhoster.com</strong>.</p>
+                        
+                        <p><strong>6. DISCLOSURE OF CUSTOMER INFORMATION</strong></p>
+                        
+                        <p><strong>DonHoster</strong> We will not disclose Customers personal and account information unless we have reason to believe that disclosing such information is necessary to identify, contact, or bring legal action against someone who may be causing harm or interfering with the rights or property of <strong>DonHoster</strong>, its Clients or others. We will disclose such Customers personal and account information if we believe in good faith that disclosure is required by law.</p>
+                        
+                        <p><strong>7. DISCLOSURE OF COMMUNICATIONS</strong></p>
+                        
+                        <p><strong>DonHoster</strong> We will not disclose to third parties (which does not include <strong>DonHoster</strong> affiliates or our officers and employees) the content of any email or other electronic communication that we store or transmit for our Clients, except when it is necessary or required to:</p>
+                        <p>Provide service to a customer</p>
+                        <p>Protect the legitimate interests of <strong>DonHoster</strong> and its Clients Cooperate with interception orders, guarantees or other legal processes that we determine, in our sole discretion, are valid and enforceable or Provide to a law enforcement agency where the contents are inadvertently obtained by us and appear to pertain to the commission of a crime.</p>
+                        <p>We disclaim any intention to censor, edit, or engage in ongoing review or surveillance of communications stored on or transmitted through the Company Network by Clients and others. However, we will review, delete or block access to communications that may harm <strong>DonHoster</strong>, its Clients or third parties based on any Acceptable Use Policy applicable to the Client.</p>
+                        
+                        <p><strong>8. CONTACT WITH DONHOSTER</strong></p>
+                        
+                        <p>Any questions regarding this Privacy Policy should be submitted through our website or by contacting our administration department via email <strong>admin@donhoster.com</strong>.</p>
+                        
+                        <p><strong>9. MODIFICATIONS OR CHANGES</strong></p>
+                        
+                        <p>We have the right to modify or change this Privacy Policy at any time without notice. Following such update, we will post the updated Privacy Policy on our website <strong>(www.donhoster.com).</strong></p>
+                        
+                        <p><strong>Last revision: March 2022</strong></p>
+      </div>
     </div>
-
-        <p className="about__pp">About <span className="about__span">DonHoster LLC,</span></p>
-        <br/>
-      
-    <section className="about__txt">
-        <p>It is a company focused on the Internet technology industry. Operating
-        since 2015, DonHoster Technology and Innovation in professional
-        dedicated servers tailored to your needs, is a provider of web hosting
-        services, domain registration, VPS, dedicated servers, colocation, SSL
-        certificate; always providing the best customer service and with a quick
-        response time for each application. It is a 100% committed company, with
-        the vision of being able to offer web hosting services to the digital
-        market with the highest quality standards and at the best price.</p>
-        <br/>
-
-        <p><span className="about__span">Founded</span> on December, 2017, it has grown from a Miami Hosting company, an
-        initiative of a group of young professionals with the desire to provide
-        companies and people in Latin America , the United States and now the
-        rest of the world, solutions to have an Internet presence. This company
-        is not only interested in offering you a product and selling it, but
-        also in advising and helping you succeed in the global Internet market.
-        It is a company specialized in business virtualization technologies and
-        the provision of Internet services accessible to individuals and
-        professionals, companies, associations and public entities. Currently,
-        its servers are located in a data center in the United States, Miami,
-        Florida, which not only has the best connectivity and hardware of the
-        highest technology, but is also supervised by our group of security
-        experts , which position it among the few hosting companies in Latin
-        America and the United States to provide a comprehensive security and
-        data protection service to our customers</p>
-        <br/>
-
-        <p><span className="about__span">Mission</span>
-        <br/>• Offer the best services and the most competitive rates to be a leader in the international community. 
-        <br/>• Maintain the necessary operational structure and minimize our costs to guarantee our customers better rates, quality and the best service. </p>
-        All this oriented to the search of appropriate technologies that guarantee our competitiveness to establish long-term working relationships with our clients. 
-        <br/>
-        <span className="about__span">Objective</span> Being a company of excellence, to provide customers with the best service in systems,servers, web designs, networks and computers in general, providing security and guarantee of our work. 
-        <br/>
-        <br/>
-
-        <span className="about__span">Values that do not characterize:</span> Ethics, Responsibility, Commitment and Respect for our clients.
-        <br/>
-        <br/>
-
-        <span className="about__span">DonHoster C.A.</span> offers its customers innovative products and services
-        designed to complement their business whether professional, independent
-        or individual. Come and become the number 1 web hosting operator in the
-        World, while maintaining high quality with our services.
-    </section>
-    </div>
-);
+     </div>
+  );
 };
 
 export default PrivacyPolicy;
--- src/Pages/TermOfServices.jsx
+++ src/Pages/TermOfServices.jsx
@@ -1,73 +1,78 @@
 import "../Pages/styles/about.css";
 import "../assets/img/banner-about.jpg";
 import images from "../Utils/images";
+import "../Pages/styles/TermOfServices.css"
 
 const TermOfServices = () => {
   return (
     <div className="about__box">
-        <h1 className="about__title">THE COMPANY</h1>
-            <h3 className="about__subtitle"> Values stem from our focus on customer well-being</h3>
-    <div className="about__p">
-        <i className="bx bx-check" /> Ethics &nbsp;
-        <i className="bx bx-check" /> Responsibility &nbsp;
-        <i className="bx bx-check" /> Respect
+      <h1 className="about__title">THE COMPANY</h1>
+      <h3 className="about__subtitle">Values stem from our focus on customer well-being</h3>
+      <div className="about__p">
+        <i className="bx bx-check"></i> Ethics &nbsp;
+        <i className="bx bx-check"></i> Responsibility &nbsp;
+        <i className="bx bx-check"></i> Respect
       </div>
+      <div className="about__banner">
+        <img src={images.bannerabout} alt="Banner About" />
+      </div>
+      <div className="term__of__services">
+        <strong>Terms of Service DonHoster</strong>
+      </div>
+      <div className="term__of">
+      <p>This document expresses the terms of service to which all customers who purchase a product from our company <strong>DonHoster</strong> agree to fully accept, and under which it is governed to provide its service. Therefore, all present or future products or services are regulated under this regulation, which in turn can be modified at any time with prior notice or not, and the user accepts said changes.</p>
+      <p><strong>RESPONSIBILITIES OF THE COMPANY</strong></p>
+      <p><strong>1. Notices</strong></p>
+      <p>The company agrees to send notifications, promotions, payment reminders and/or promotions, through tickets or email. It should be noted that in the case of payment reminders, despite the fact that the company undertakes to notify them, it is the customers duty to be aware of their expiration dates and/or renewal of services.
+For this reason, it is of the utmost importance that the client commits to supplying their contact data, current and updated, so that in this way they can effectively receive our notices, in turn notify us if they change for whatever reason, remaining exempts the company from responsibility, if an email does not reach its destination, for the reasons mentioned above.</p>
+      <p><strong>2. Technical support</strong></p>
+      <p>The company undertakes to provide technical support exclusively by ticket or email, only for problems related to hardware and server connectivity, but not support linked to the Software, understanding that the client is the only administrator of his product, its security, operability and the presence of backup of its content. We do not provide technical support via telephone, nor through SOCIAL NETWORKS.</p>
+      <p><strong>3. Warranty</strong></p>
+      <p>We cannot provide a guarantee that the service offered may not be interrupted or free of errors, that the information available from our products is free of viruses, it being understood that the client knows in advance all the dangers inherent in the use of the technologies, and the vulnerabilities they have. The client accepts and is aware that all information placed on our servers could be available to any internet user, and that they have no responsibility with us, that is, we are not responsible for the loss of your information.</p>
+      <p><strong>4. Prices</strong></p>
+      <p>The company is fully free to modify the price of its products at any time it wishes, whether it is the value to acquire a new service or, for its renewal; with or without prior notice to its customers.</p>
+      <p>CUSTOMER RESPONSIBILITIES</p>
+      <p><strong>1. Use of our products</strong></p>
+<p>The client agrees to use our products, only for legal purposes, that is, any illicit conduct or that the company considers improper, in accordance with the provisions of international standards is strictly prohibited, and the company is in all your right to analyze and make decisions or sanctions depending on the situation, even reaching the temporary or total termination of the service contract.
+The client accepts that he is fully responsible, and excludes our company, for any content in the products purchased by him, the use that is made of it, and the international legal sanctions that these may incur.</p>
 
-    <div className="about__banner">
-        <img src={images.bannerabout} alt="" />
+<p><strong>2. Infractions</strong></p>
+<p>The client agrees to have a respectful conduct towards our company and its employees; Considering that any action in favor of deteriorating our image, mistreating, threatening or offending, will be sufficient reason for the immediate completion of all the services provided by our company.
+Likewise, the client agrees not to execute any voluntary or involuntary action that harms any of our servers, affecting the free operation of it for other clients.</p>
+
+<p><strong>3. Product Suspension</strong></p>
+<p>The client understands that the non-cancellation of the value of the product before the expiration date is sufficient reason for its suspension, leaving our company a period of 72 hours for the client to communicate with our company, before it is deleted all your information in the product. If the client wishes to cancel his product, or not to continue renewing it, he must notify it through a cancellation request, with his respective justification.</p>
+
+<p><strong>4. Domain Acquisition</strong></p>
+<p>The client understands that the availability of a domain name is not controlled by our company, so it is the clients responsibility to wait until a registration confirmation is made. Likewise, this may not be modified until the contract is completed, so any error in your transcription or change of opinion by the client will be your responsibility; so if you want to change the name of your domain, you must register and acquire a new one, which is available.</p>
+
+<p><strong>5. Renewals</strong></p>
+<p>It is the clients knowledge that expired domains, which were renewed late, will have a limit of up to 10 days, which will take between 24 and 48 hours to reactivate due to the required propagation, and that they may not be available, understanding that said period of time is not controlled by us.</p>
+
+<p><strong>6. Domain service limitations</strong></p>
+<p>It is known to the client that the registration as well as the use of a domain can be canceled, suspended, limited or transferred, if our company, the domain provider or even if ICANN, the government or any other relevant entity, so consider, whether for reasons of dispute resolution, protection of your security, claims for infringement of intellectual property, among others.</p>
+
+<p><strong>7. User Identification</strong></p>
+<p>The client claims to be a legally responsible person, to be over 18 years of age or to have the authorization of their representative, the latter being legally responsible for the use given to this product and the information uploaded there. On the other hand, it also ensures that the information you provide us, email, name, telephone number or other data that is necessary, is true and that you will keep it updated. Finally, the client through this document also accepts the policies of treatment of their data, as well as the publication of information in the WHOIS database and its use by the company, service providers or government entities that require, <a href="https://www.donhoster.com/privacy-policy/">Privacy Policies</a>.</p>
+
+<p><strong>8. Legality of the Registry</strong></p>
+<p>The client understands that the acceptance of the registration request, the registration and even the beginning of the provision of the service, are not indicative that we or any other entity, including the government, consider its registration or use legal. None of the aforementioned actions are a sufficient basis to validate that it is legal, that it is not infringing a rule or regulation. Therefore, the client must commit that the use of the domain(s) provided does not violate any regulation of the company or international entity, and that it will not be used for malicious, harmful or fraudulent purposes.</p>
+
+<p><strong>9. Disputes and Conflict resolution</strong></p>
+<p>The client releases our company, its employees and its suppliers, from all responsibility for any demand, damage or any legal charge associated with the registration or management of your domain.
+Likewise, the client has the commitment and agrees to notify our company in advance, if they have any inconvenience or claim with any of our services, in order to seek a conciliation or solution, through the ticket system or via email.</p>
+
+<p><strong>10. Refund</strong></p>
+<p>The client will only receive a refund of the value paid for the service purchased, only when it has not been successfully received 72 hours after the formulation of the ticket, payment and confirmation of the order; and our company has not been in communication with the client to justify the reason for said situation, understanding that there is a variety of conflicts that may arise in the use of technology, which may or may not be the responsibility of <strong>DonHoster</strong>.</p>
+
+<p><strong>Final note</strong>: Remember that if you do not accept these terms, then <strong>DonHoster</strong> is within his rights not to want to provide his service and sale of products. If you do not agree with our policies, please refrain from purchasing them.</p>
+
+
+
+      <p><strong>Last revision: March 2022</strong></p>
+      </div>
     </div>
-
-        <p className="about__pp">About <span className="about__span">DonHoster LLC,</span></p>
-        <br/>
-      
-    <section className="about__txt">
-        <p>It is a company focused on the Internet technology industry. Operating
-        since 2015, DonHoster Technology and Innovation in professional
-        dedicated servers tailored to your needs, is a provider of web hosting
-        services, domain registration, VPS, dedicated servers, colocation, SSL
-        certificate; always providing the best customer service and with a quick
-        response time for each application. It is a 100% committed company, with
-        the vision of being able to offer web hosting services to the digital
-        market with the highest quality standards and at the best price.</p>
-        <br/>
-
-        <p><span className="about__span">Founded</span> on December, 2017, it has grown from a Miami Hosting company, an
-        initiative of a group of young professionals with the desire to provide
-        companies and people in Latin America , the United States and now the
-        rest of the world, solutions to have an Internet presence. This company
-        is not only interested in offering you a product and selling it, but
-        also in advising and helping you succeed in the global Internet market.
-        It is a company specialized in business virtualization technologies and
-        the provision of Internet services accessible to individuals and
-        professionals, companies, associations and public entities. Currently,
-        its servers are located in a data center in the United States, Miami,
-        Florida, which not only has the best connectivity and hardware of the
-        highest technology, but is also supervised by our group of security
-        experts , which position it among the few hosting companies in Latin
-        America and the United States to provide a comprehensive security and
-        data protection service to our customers</p>
-        <br/>
-
-        <p><span className="about__span">Mission</span>
-        <br/>• Offer the best services and the most competitive rates to be a leader in the international community. 
-        <br/>• Maintain the necessary operational structure and minimize our costs to guarantee our customers better rates, quality and the best service. </p>
-        All this oriented to the search of appropriate technologies that guarantee our competitiveness to establish long-term working relationships with our clients. 
-        <br/>
-        <span className="about__span">Objective</span> Being a company of excellence, to provide customers with the best service in systems,servers, web designs, networks and computers in general, providing security and guarantee of our work. 
-        <br/>
-        <br/>
-
-        <span className="about__span">Values that do not characterize:</span> Ethics, Responsibility, Commitment and Respect for our clients.
-        <br/>
-        <br/>
-
-        <span className="about__span">DonHoster C.A.</span> offers its customers innovative products and services
-        designed to complement their business whether professional, independent
-        or individual. Come and become the number 1 web hosting operator in the
-        World, while maintaining high quality with our services.
-    </section>
-    </div>
-);
+  );
 };
 
 export default TermOfServices;
