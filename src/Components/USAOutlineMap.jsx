c22d3072b8742bc029dc69fb050abbdf9c5a83b0
Cristobal <quilimacox1@gmail.com>
2023-11-22 14:51:48 -0500

    Se finaliza componente datacenter map

--- src/Components/USAOutlineMap.jsx
+++ src/Components/USAOutlineMap.jsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
 import usaOutline from "../assets/img/USA-Map-PNG.png"; // Importa la imagen del contorno de Estados Unidos
 import "../Components/styles/USAOutlineMap.css"
+import useScrollEffect from "../hooks/useScrollEffect";
+import "../Components/styles/Services.css"
 
 const USAOutlineMap = () => {
+
+    useScrollEffect(['.animatin'], 60)
+
   return (
     <div className="principal__USAOutlineMap">
-        <p className="location_title"><span className="color__location">Location</span> Matters</p>
-        <p className="info__location">Our Miami data centers are at the heart of one of the worlds largest Internet exchanges interconnecting Latin America, the Northeast US and Europe. Provides exceptional hosting solutions offering a high level of infrastructure and connectivity from Miami with innovative and automated technology for small and large businesses offering cloud-based services and guaranteed quality and availability of hosting services and best available hardware distributed in multiple centers of data.</p>
-    <div className="usa-map-container">
+        <p className="location_title animatin"><span className="color__location">Location</span> Matters</p>
+        <p className="info__location animatin">Our Miami data centers are at the heart of one of the worlds largest Internet exchanges interconnecting Latin America, the Northeast US and Europe. Provides exceptional hosting solutions offering a high level of infrastructure and connectivity from Miami with innovative and automated technology for small and large businesses offering cloud-based services and guaranteed quality and availability of hosting services and best available hardware distributed in multiple centers of data.</p>
+    <div className="usa-map-container animatin">
     <i className='bx bxs-location-plus location__icon__btn'></i>
         <img src={usaOutline} alt="USA Outline" className="usa-outline" />
     </div>
