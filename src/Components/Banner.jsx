8d291891a9736e2a552f42a1454b68c9a6a700c2
Cristobal <quilimacox1@gmail.com>
2024-10-28 21:15:02 -0500

    Se ajusta codigo de seo de la pagina web principal

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,26 +1,47 @@
 import { Link } from "react-router-dom";
-import "../Components/styles/Banner.css"
+import "../Components/styles/Banner.css";
 import images from "../Utils/images";
 
 const Banner = () => {
   return (
-    <div className="container container__banner1">
-      <img className="img__banner__data" src={images.bannerdata} alt="data center" width="100%" height="400" />
+    <div className="container container__banner1" style={{ width: '100%' }}>
+      <img 
+        className="img__banner__data" 
+        src={images.bannerdata} 
+        alt="Banner with data" 
+        width="100%" 
+        height="400" 
+      />
       <div className="row align-items-center">
-        <div className="col-xl-6 col-lg-6 wow fadeInDown" data-wow-delay="0.1s" style={{ visibility: 'visible', animationDelay: '0.1s', animationName: 'fadeInDown' }}>
+        <div 
+          className="col-xl-6 col-lg-6 wow fadeInDown" 
+          data-wow-delay="0.1s" 
+          style={{ visibility: 'visible', animationDelay: '0.1s', animationName: 'fadeInDown' }}
+        >
           <div className="banner-content">
-          <h1 className="principal_title">Dedicated <span className="text-base">Server</span> &amp; <span className="text-base-2">VPS Hosting</span> in Our Platform
+            <h1 className="principal_title">
+              Dedicated <span className="text-base">Server </span> &amp; <span className="text-base-2">VPS Hosting </span>in Our Platform
             </h1>
             <p className="principal_description">
-            Our network of data centers in , USA, Miami.
-Connectivity at 99% uptimice, Integrated DDoS protection
+              Our network of data centers in Miami, USA. Connectivity at 99% uptime, Integrated DDoS protection.
             </p>
-            <Link to="/dedicated-servers"><button className="btn__to__server">See Characteristics of Our Plans</button></Link>
+            <Link to="/dedicated-servers">
+              <button className="btn__to__server">See Characteristics of Our Plans</button>
+            </Link>
           </div>
         </div>
         <div className="col-xl-6 col-lg-6">
-          <div className="banner-thumb wow fadeInRight" style={{ visibility: 'visible', animationName: 'fadeInRight' }}>
-          <img className="img_banner" src={images.server1} alt="Server in Miami" width="600" height="400" />
+          <div 
+            className="banner-thumb wow fadeInRight" 
+            style={{ visibility: 'visible', animationName: 'fadeInRight' }}
+          >
+            <img 
+              className="img_banner" 
+              src={images.server1} 
+              alt="Server in Miami" 
+              width="100%" 
+              height="400" 
+            />
           </div>
         </div>
       </div>
