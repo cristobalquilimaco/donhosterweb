9643d732a336363ac30ab3b8ac3cee307bf5c20c
Cristobal <quilimacox1@gmail.com>
2024-10-28 21:13:08 -0500

    Se ajusta seo en banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -9,19 +9,18 @@ const Banner = () => {
       <div className="row align-items-center">
         <div className="col-xl-6 col-lg-6 wow fadeInDown" data-wow-delay="0.1s" style={{ visibility: 'visible', animationDelay: '0.1s', animationName: 'fadeInDown' }}>
           <div className="banner-content">
-            <h1 className="principal_title">Dedicated <span className="text-base">Server </span> &amp; <span className="text-base-2">VPS Hosting </span>in Our
-              Platform
+          <h1 className="principal_title">Dedicated <span className="text-base">Server</span> &amp; <span className="text-base-2">VPS Hosting</span> in Our Platform
             </h1>
             <p className="principal_description">
             Our network of data centers in , USA, Miami.
 Connectivity at 99% uptimice, Integrated DDoS protection
             </p>
-            <Link to="/dedicated-servers"><button className="btn__to__server">see characteristics of our plans</button></Link>
+            <Link to="/dedicated-servers"><button className="btn__to__server">See Characteristics of Our Plans</button></Link>
           </div>
         </div>
         <div className="col-xl-6 col-lg-6">
           <div className="banner-thumb wow fadeInRight" style={{ visibility: 'visible', animationName: 'fadeInRight' }}>
-          <img className="img_banner" src={images.server1} alt="vpmkvm miami" width="600" height="400" />
+          <img className="img_banner" src={images.server1} alt="Server in Miami" width="600" height="400" />
           </div>
         </div>
       </div>
