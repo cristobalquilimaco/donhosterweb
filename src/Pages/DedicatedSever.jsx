b1b159eb2a318014fd90bf1652300cd2841351f5
Cristobal <quilimacox1@gmail.com>
2024-10-29 20:20:39 -0500

    Se inicia pantalla de carga

--- src/Pages/DedicatedSever.jsx
+++ src/Pages/DedicatedSever.jsx
@@ -15,14 +15,14 @@ const DedicatedServer = () => {
       <div>
         <Helmet>
           <title>Miami Dedicated Server - Bare Metal Servers | DonHoster</title>
-          <meta 
-            name="description" 
-            content="Discover high-performance dedicated servers in Miami with DonHoster. Enjoy exclusive physical servers and reliable connectivity." 
+          <meta
+            name="description"
+            content="Discover high-performance dedicated servers in Miami with DonHoster. Enjoy exclusive physical servers and reliable connectivity."
           />
           <meta name="keywords" content="dedicated servers, bare metal servers, dedicated hosting, Miami data center, high-performance servers, secure servers, business hosting solutions" />
           <meta name="robots" content="index, follow" />
           <link rel="canonical" href="https://www.donhoster.com/dedicated-servers" />
-          
+
           <meta property="og:title" content="Miami Dedicated Server - Bare Metal Servers | DonHoster" />
           <meta property="og:description" content="High-performance dedicated servers in Miami tailored for your business needs." />
           <meta property="og:url" content="https://www.donhoster.com/dedicated-servers" />
@@ -30,10 +30,8 @@ const DedicatedServer = () => {
           <meta property="og:image:alt" content="High-Performance Dedicated Servers in Miami" />
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="630" />
-
           <meta name="google-site-verification" content="google94b194ab78fb2b51.html" />
           <meta name="ahrefs-site-verification" content="b1b407b17742f4b1f2511abaa6ef60a204af93779773732963c4500166c3efaa" />
-
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-E8HRCWRKGG"></script>
           <script>
             {`
@@ -43,7 +41,6 @@ const DedicatedServer = () => {
               gtag('config', 'G-E8HRCWRKGG');
             `}
           </script>
-
           <script type="application/ld+json">
             {`
               {
@@ -59,7 +56,6 @@ const DedicatedServer = () => {
             `}
           </script>
         </Helmet>
-
         <main className="about__box">
           <h1 className="about__title">Dedicated Servers</h1>
           <h3 className="about__subtitle">
@@ -70,12 +66,10 @@ const DedicatedServer = () => {
             <i className="bx bx-check" /> 99.9% Uptime Guarantee &nbsp;
             <i className="bx bx-check" /> Support for Windows & Linux
           </div>
-
           <div className="about__banner">
-            <img src={images.bannerabout} alt="High-Performance Dedicated Server Banner" />
+            <img src={images.bannerabout} alt="High-Performance Dedicated Server Banner" width="100%" height="auto" />
           </div>
         </main>
-
         <section className="pricing-section pt-120 pb-120">
           <div className="container">
             <h2 className="section-title">Our Dedicated Server Plans</h2>
@@ -103,7 +97,6 @@ const DedicatedServer = () => {
                 </article>
               ))}
             </div>
-
             <section className="section__skill">
               <h2 className="features">The Best Features at the Best Price!</h2>
               <div className="description_feat">
@@ -113,7 +106,7 @@ const DedicatedServer = () => {
                   Dedicated server hosting provides unparalleled control and resources for your online ventures. With a dedicated server, you have exclusive access to an entire server, ensuring optimal performance. Choose DonHoster for reliability and performance.
                 </p>
               </div>
-              <FAQSection/>
+              <FAQSection />
               <div className="animatin features__skill">
                 <ul className="list__skill">
                   <li><i className="skill__icon bx bxs-rocket"></i></li>
@@ -132,13 +125,9 @@ const DedicatedServer = () => {
                 </ul>
               </div>
             </section>
-            
           </div>
-          
         </section>
-        
       </div>
-      
     </HelmetProvider>
   );
 };
